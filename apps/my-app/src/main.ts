1be59faa27a2d6142aee7e3aee8185f380a9b94f
letier3110 <letier3110@gmail.com>
2022-07-19 00:10:18 +0100

    add update

--- apps/my-app/src/main.ts
+++ apps/my-app/src/main.ts
@@ -11,6 +11,8 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to my-app!' });
 });
 
+console.log('update time!');
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
